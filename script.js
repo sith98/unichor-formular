8f64ecb389b32d8e398cf437c67782b920a520fe
Simon Thelen <sith98@web.de>
2023-11-07 16:19:40 +0100

    Add Email feature

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const loadData = () => {
+    const email = document.querySelector("#email").value.trim();
     const firstname = document.querySelector("#firstname").value.trim();
     const lastname = document.querySelector("#lastname").value.trim();
     const birthday = document.querySelector("#birthday").value;
     const voiceGroup = document.querySelector("input[name=voice-group]:checked").value;
 
-    return { firstname, lastname, birthday, voiceGroup };
+    return { email, firstname, lastname, birthday, voiceGroup };
 }
 
 const main = () => {
     const data = JSON.parse(localStorage.getItem("data"));
     // autofill form
     if (data) {
+        document.querySelector("#email").value = data.email;
         document.querySelector("#firstname").value = data.firstname;
         document.querySelector("#lastname").value = data.lastname;
         document.querySelector("#birthday").value = data.birthday;
@@ -24,17 +26,17 @@ const main = () => {
         localStorage.setItem("data", JSON.stringify({ ...data, redirect: false }));
         location.pathname += "redirect";
     });
-    document.querySelector("#copy").addEventListener("click", evt => {
-        evt.preventDefault();
-        // get form data
-        const data = loadData();
-        const url = `https://docs.google.com/forms/d/e/1FAIpQLSexAQunwzbLiapgd0MPSxpqvvuH8msvc8CWK544I6ghlr-qMg/viewform?entry.1345550671=${data.lastname},+${data.firstname}&entry.945327328=${data.birthday}&entry.1445587767=${data.voiceGroup}`;
+    // document.querySelector("#copy").addEventListener("click", evt => {
+    //     evt.preventDefault();
+    //     // get form data
+    //     const data = loadData();
+    //     const url = `https://docs.google.com/forms/d/e/1FAIpQLSexAQunwzbLiapgd0MPSxpqvvuH8msvc8CWK544I6ghlr-qMg/viewform?entry.1345550671=${data.lastname},+${data.firstname}&entry.945327328=${data.birthday}&entry.1445587767=${data.voiceGroup}`;
 
-        navigator.clipboard.writeText(url).then(() => {
-            const toast = document.querySelector(".toast");
-            toast.style.display = "block";
-        })
-    });
+    //     navigator.clipboard.writeText(url).then(() => {
+    //         const toast = document.querySelector(".toast");
+    //         toast.style.display = "block";
+    //     })
+    // });
 }
 
 window.addEventListener("DOMContentLoaded", main);
