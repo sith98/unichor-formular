a76c549a5a632cd15fa4c43b46e1ab8c51fef75e
Simon Thelen <sith98@web.de>
2023-11-07 14:33:09 +0100

    Add "copy link" button

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
+const loadData = () => {
+    const firstname = document.querySelector("#firstname").value;
+    const lastname = document.querySelector("#lastname").value;
+    const birthday = document.querySelector("#birthday").value;
+    const voiceGroup = document.querySelector("input[name=voice-group]:checked").value;
+
+    return { firstname, lastname, birthday, voiceGroup };
+}
+
 const main = () => {
     document.querySelector("#submit").addEventListener("click", evt => {
         evt.preventDefault();
         // get form data
-        const firstname = document.querySelector("#firstname").value;
-        const lastname = document.querySelector("#lastname").value;
-        const birthday = document.querySelector("#birthday").value;
-        const voiceGroup = document.querySelector("input[name=voice-group]:checked").value;
+        const data = loadData();
 
-        localStorage.setItem("data", JSON.stringify({
-            firstname: firstname,
-            lastname: lastname,
-            birthday: birthday,
-            voiceGroup: voiceGroup
-        }));
+        localStorage.setItem("data", JSON.stringify(data));
         window.open("/redirect", "_self")
+    });
+    document.querySelector("#copy").addEventListener("click", evt => {
+        evt.preventDefault();
+        // get form data
+        const data = loadData();
+        const url = `https://docs.google.com/forms/d/e/1FAIpQLSexAQunwzbLiapgd0MPSxpqvvuH8msvc8CWK544I6ghlr-qMg/viewform?entry.1345550671=${data.lastname},+${data.firstname}&entry.945327328=${data.birthday}&entry.1445587767=${data.voiceGroup}`;
+
+        navigator.clipboard.writeText(url).then(() => {
+            const toast = document.querySelector(".toast");
+            toast.style.display = "block";
+        })
     });
 }
 
